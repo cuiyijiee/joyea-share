e88c39a4f8c9e739310e65b78669219c8297df08
cuiyijie <cyj976655@gmail.com>
2020-11-12 00:06:48 +0800

    改进排行榜UI

--- mobile/src/views/Leaderboard.vue
+++ mobile/src/views/Leaderboard.vue
@@ -24,23 +24,28 @@
                 </van-tabs>
             </div>
         </div>
-        <div style="height: 120px;text-align: center;color: #ffffff;"
-             :style="{background: 'url('+bg_leaderboard+')'}"
-             v-if="leaderObjType === 0">
-            <div style="font-size: 20px;">我的排名</div>
-            <div style="font-size: 50px;" v-if="myScore > 0">{{ myIndex }}</div>
+        <div
+            style="height: 120px;text-align: center;color: #ffffff;"
+            :style="{background: 'url('+bg_leaderboard+')','background-size':'cover','background-repeat': 'no-repeat'}"
+            v-if="leaderObjType === 0">
+            <div style="font-size: 20px; padding-top: 20px">我的排名
+                <van-icon style="margin-top: 2px" name="question" @click="tipVisible = true"/>
+            </div>
+            <div v-if="myScore > 0">
+                <span style="font-size: 40px;">第{{ myIndex }}名</span>
+                <span> ( 积分: {{ myScore }}分 )</span>
+            </div>
             <div style="font-size: 50px;" v-else> -</div>
-            <div style="font-size: 20px;">我的得分: {{ myScore }}</div>
         </div>
-        <van-row style="text-align: center;color: #8c939d;padding: 8px 0">
-            <van-col span="4">名次</van-col>
-            <van-col span="16">名称</van-col>
-            <van-col span="4">得分</van-col>
-        </van-row>
         <div v-if="leaderObjType === 0">
-            <div v-for="(item,index) in leaderboardList.slice(0,10)">
-                <van-row style="text-align: center">
-                    <van-col span="4">
+            <van-row style="text-align: center;color: #8c939d;padding: 8px 0">
+                <van-col span="6">排名</van-col>
+                <van-col span="12">用户名</van-col>
+                <van-col span="6">积分</van-col>
+            </van-row>
+            <div v-for="(item,index) in leaderboardList.slice(0,10)" style="margin: 10px 0">
+                <van-row>
+                    <van-col span="6" style="text-align: center">
                         <div style="margin-top: 10px" v-if="index < 3">
                             <van-image v-if="index === 0" width="30" height="30" :src="medal_gold"/>
                             <van-image v-if="index === 1" width="30" height="30" :src="medal_silver"/>
@@ -50,7 +55,7 @@
                             {{ index + 1 }}
                         </div>
                     </van-col>
-                    <van-col span="16" style="height: 50px">
+                    <van-col span="12" style="height: 50px">
                         <div v-if="item.value > 0">
                             <div>{{ item.user.joyeaName }}</div>
                             <div style="color: #8c939d">{{ item.user.department + ' | ' + item.user.position }}</div>
@@ -59,17 +64,22 @@
                             -- 虚位以待 --
                         </div>
                     </van-col>
-                    <van-col span="4">
+                    <van-col span="6" style="text-align: center">
                         <div style="margin-top: 15px">{{ item.value }}</div>
                     </van-col>
                 </van-row>
-                <div style="height: 1px;background: #99a9bf"/>
+                <div style="height: 1px;background: #eeeeee"/>
             </div>
         </div>
         <div v-else>
-            <div v-for="(item,index) in leaderboardDepartmentList.slice(0,10)">
-                <van-row style="text-align: center">
-                    <van-col span="4">
+            <van-row style="text-align: center;color: #8c939d;padding: 8px 0">
+                <van-col span="6">排名</van-col>
+                <van-col span="12">部门</van-col>
+                <van-col span="6">积分</van-col>
+            </van-row>
+            <div v-for="(item,index) in leaderboardDepartmentList.slice(0,10)" style="margin: 10px 0">
+                <van-row>
+                    <van-col span="6" style="text-align: center">
                         <div style="margin-top: 10px" v-if="index < 3">
                             <van-image v-if="index === 0" width="30" height="30" :src="medal_gold"/>
                             <van-image v-if="index === 1" width="30" height="30" :src="medal_silver"/>
@@ -79,7 +89,7 @@
                             {{ index + 1 }}
                         </div>
                     </van-col>
-                    <van-col span="16" style="height: 50px">
+                    <van-col span="12" style="height: 50px">
                         <div v-if="item.value > 0" style="margin-top: 10px;">
                             <div>{{ item.department }}</div>
                         </div>
@@ -87,13 +97,30 @@
                             -- 虚位以待 --
                         </div>
                     </van-col>
-                    <van-col span="4">
+                    <van-col span="6" style="text-align: center">
                         <div style="margin-top: 15px">{{ item.value }}</div>
                     </van-col>
                 </van-row>
-                <div style="height: 1px;background: #99a9bf"/>
+                <div style="height: 1px;background: #eeeeee"/>
             </div>
         </div>
+        <van-dialog v-model="tipVisible" :title="tipTitle" show-cancel-button theme="round" :showCancelButton="false">
+            <div class="content" style="padding: 0px 20px">
+                <div v-if="leaderBoardType === 0">
+                    <p>统计每个人被采纳的素材数量，但是会对被采纳的素材进行分类、采用积分的方式进行统计，照片、视频等未加工的通用素材积1分，友商对比表、细分市场介绍资料等经过整理分析形成的专用素材积5分。</p>
+                    <p>如：友商报价书、合同等，积1分，如果进一步将此信息整理成友商与我司的对比表，则积5分。</p>
+                </div>
+                <div v-else-if="leaderBoardType === 1">
+                    <p>统计每个人被采纳的素材被其他人引用或者下载的次数，这是对素材质量的排名。</p>
+                </div>
+                <div v-else-if="leaderBoardType === 2">
+                    <p>统计每个人分享到清单广场的清单数量。</p>
+                </div>
+                <div v-else-if="leaderBoardType === 3">
+                    <p>统计每个人分享到清单广场的清单被其他人收藏的次数，这是对清单质量的排名。</p>
+                </div>
+            </div>
+        </van-dialog>
     </div>
 </template>
 
@@ -128,10 +155,23 @@ export default {
             leaderboardDepartmentList: [],
 
             myScore: 0,
-            myIndex: 0
+            myIndex: 0,
+
+            tipVisible: false
         }
     },
     computed: {
+        tipTitle:function () {
+            if (this.leaderBoardType === 0) {
+                return "素材采纳排名规则";
+            } else if (this.leaderBoardType === 1) {
+                return "素材引用排名规则";
+            } else if (this.leaderBoardType === 2) {
+                return "清单采纳排名规则";
+            } else {
+                return "清单收藏排名规则";
+            }
+        },
         ...mapGetters([
             'userInfo'
         ])
@@ -158,6 +198,7 @@ export default {
             for (let index = 2020; index <= currentYear; index++) {
                 this.option.year.push({text: index + '年', value: index});
             }
+            this.year = currentYear;
             this.month = Number(moment(new Date()).format('MM'));
         },
         handleGetAlbumShare() {
