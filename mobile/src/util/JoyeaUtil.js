2720a64ac0ee57d1d0de2463b65ef47faba783cd
yjcui3 <yjcui3@iflytek.com>
2022-06-17 17:32:53 +0800

    fix:修复bug

--- mobile/src/util/JoyeaUtil.js
+++ mobile/src/util/JoyeaUtil.js
@@ -5,7 +5,7 @@ import {previewFile} from "@/api";
 
 export function genSrcPreviewSrc(neid, hash, rev, previewType, sessionId) {
     return 'https://console.box.lenovo.com/v2/preview_router?type=' + previewType + '&root=databox&path=&path_type=ent&from=&neid='
-        + neid + '&hash=' + hash + '&rev=' + rev + "&X-LENOVO-SESS-ID=" + sessionId + "&xxoo=" + new Date().getTime();
+        + neid + "&rev=&X-LENOVO-SESS-ID=" + sessionId + "&xxoo=" + new Date().getTime();
 }
 
 export function genSrcOriginSrc(path, neid, sessionId) {
